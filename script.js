41e027ea4416ec8e45f86d145dab7e915a0d016f
Aaliyan Gul <aaliyan111@gmail.com>
2019-03-09 15:33:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-<script>
-jQuery.ajax( {
-  url: '//freegeoip.net/json/',
-  type: 'POST',
-  dataType: 'jsonp',
-  success: function(location) {
-    // UK -- United Kingdom
-    if (location.country_code === 'AU') {
-      // Redirect visitor to the UK store
-      window.top.location.href = 'https://au.dittybird.com/';
-    } else {
-      // Redirect visitor to the Worldwide
-      window.top.location.href = 'https://www..dittybird.com/';
+<script type="text/javascript">
+  //Coded by Codehouse - Aaliyan Gul
+  jQuery.ajax({
+    url: 'https://ipapi.co/json',
+    type: 'POST',
+    async: false,
+    contentType: "application/json; charset=utf-8",
+    dataType: 'json',
+    success: function (location) {
+      // AU -- Australia
+      if (location.country === 'AU') { // Redirect visitor to the AU store
+          window.top.location.href = 'https://au.dittybird.com/';
+      } else if (location.country === 'DE') { // Redirect visitor to the UK store
+          window.top.location.href = 'https://de.dittybird.com/';
+      }
     }
-  }
-});
+  });
 </script>
