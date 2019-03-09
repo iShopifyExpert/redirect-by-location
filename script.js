7008832059d0e6c9c66f05f50fbbdb10387a12bf
Aaliyan Gul <aaliyan111@gmail.com>
2019-03-09 14:59:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ jQuery.ajax( {
   dataType: 'jsonp',
   success: function(location) {
     // UK -- United Kingdom
-    if (location.country_code === 'UK') {
+    if (location.country_code === 'AU') {
       // Redirect visitor to the UK store
-      window.top.location.href = 'http://uk.blackclaw.com';
+      window.top.location.href = 'https://au.dittybird.com/';
+    } else {
+      // Redirect visitor to the Worldwide
+      window.top.location.href = 'https://www..dittybird.com/';
     }
   }
 });
